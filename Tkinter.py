5dace049886fd929f78c9d25057d82ef55824eae
Peter-Szilard Nemeth <Peter-Szilard.Nemeth@heitec.hu>
2020-06-16 15:20:09 +0200

    grid

--- Tkinter.py
+++ Tkinter.py
@@ -3,8 +3,8 @@ from tkinter import *
 root = Tk()
 
 #Creating a Label Widget
-myLabel = Label(root, text = "Hello World!")
+myLabel1 = Label(root, text = "Hello World!").grid(row = 1, column = 0)
+myLabel2 = Label(root, text = "Bullshit").grid(row = 1, column = 2)
 
-myLabel.pack()
 
 root.mainloop()
