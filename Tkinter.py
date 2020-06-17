1ae8cc8e8f0b7a27eea36a619cd49935f56c93e1
Peter-Szilard Nemeth <Peter-Szilard.Nemeth@heitec.hu>
2020-06-17 14:51:16 +0200

    Entry

--- Tkinter.py
+++ Tkinter.py
@@ -3,8 +3,14 @@ from tkinter import *
 root = Tk()
 
 def myClick():
-    myLable = Label(root, text="Clicked")
+    myLable = Label(root, text=e.get())
     myLable.pack()
+
+e = Entry(root, width = 100)
+e.get()
+e.pack()
+e.insert(0, "Blabla: ")
+
 
 myButton = Button(root, text ="Click me!", pady=50, padx = 100, command=myClick, fg="red")
 myButton.pack()
