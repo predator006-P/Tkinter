ab985317b6ad23b53022dfd5e078d048f345f751
Peter-Szilard Nemeth <Peter-Szilard.Nemeth@heitec.hu>
2020-06-18 10:27:36 +0200

    vége

--- Tkinter.py
+++ Tkinter.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
 root = Tk()
-
+root.title('ZF message catalog')
+root.iconbitmap('C:/Users/pnemeth/Downloads/zf_official_logo_svg_TVG_icon.ico')
 def myClick():
     myLable = Label(root, text=e.get())
     myLable.pack()
@@ -11,8 +12,9 @@ e.get()
 e.pack()
 e.insert(0, "Blabla: ")
 
-
 myButton = Button(root, text ="Click me!", pady=50, padx = 100, command=myClick, fg="red")
+button_quit = Button(root, text = "Close", command = root.quit)
+button_quit.pack()
 myButton.pack()
 
 root.mainloop()
